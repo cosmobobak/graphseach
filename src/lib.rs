e08a8f30ef175989987a0cc6a7dac5c9ad64ba5f
Cosmo Bobak <s2079150@reerslev.inf.ed.ac.uk>
2022-01-24 12:05:01 +0000

    add 2 gamut

--- src/lib.rs
+++ src/lib.rs
@@ -17,22 +17,25 @@ pub fn gamut<G: graph::Graph>(game: &G) {
     let mut depthfirst = dfs::DFS::new();
     let mut it_deep = dfs::IterDeepening::new();
     println!(
-        "bfs finds the solution {} \n bfs expands {} nodes. \n the largest frontier maintained was {} nodes.", 
+        "bfs finds the solution {} \n bfs expands {} nodes. \n bfs finds the path {}. \n the largest frontier maintained was {} nodes.", 
         breadthfirst.search_tracked(game, G::root())
             .expect("bfs failed to find a solution"),
         breadthfirst.nodes_visited(),
-        breadthfirst.max_frontier()
+        breadthfirst.max_frontier(),
+        breadthfirst.path()
     );
     println!(
-        "dfs finds the solution {} \n dfs expands {} nodes.", 
+        "dfs finds the solution {} \n dfs expands {} nodes. \n bfs finds the path {}.", 
         depthfirst.search_tracked(game, G::root())
             .expect("dfs failed to find a solution"),
-        depthfirst.nodes_visited()
+        depthfirst.nodes_visited(),
+        depthfirst.path()
     );
     println!(
-        "iterative deepening dfs finds the solution {} \n iterative deepening dfs expands {} nodes.", 
+        "iterative deepening dfs finds the solution {} \n iterative deepening dfs expands {} nodes. \n bfs finds the path {}.", 
         it_deep.search_tracked(game, G::root())
             .expect("iterative deepening dfs failed to find a solution"),
-        it_deep.nodes_visited()
+        it_deep.nodes_visited(),
+        it_deep.path()
     );
 }
