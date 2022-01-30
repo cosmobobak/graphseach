42d8a373740fe98f0c9ff421ddec85139aba1543
cosmobobak <cosmobobak@gmail.com>
2022-01-30 23:57:43 +0000

    add path_cost for weighted graphs

--- src/graph.rs
+++ src/graph.rs
@@ -15,6 +15,10 @@ pub trait Graph {
 #[allow(clippy::module_name_repetitions)]
 pub trait WeightedGraph: Graph {
     fn edge_weight(&self, from: Self::Node, to: Self::Node) -> i64;
+
+    fn path_cost(&self, path: &[Self::Node]) -> i64 {
+        path.iter().zip(path.iter().skip(1)).map(|(a, b)| self.edge_weight(*a, *b)).sum()
+    }
 }
 
 #[allow(clippy::module_name_repetitions)]
